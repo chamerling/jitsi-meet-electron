6f6694d8fa6c943276a6ac7a8686b31df979aa57
Christophe HAMERLING <chamerling@linagora.com>
2020-04-30 15:04:14 +0200

    Allow deeplinks for any jitsi instance

--- app/features/app/components/App.js
+++ app/features/app/components/App.js
@@ -68,20 +68,16 @@ class App extends Component<*> {
      * @returns {void}
      */
     _listenOnProtocolMessages(event, inputURL: string) {
-        let room;
-
         // Remove trailing slash if one exists.
-        if (inputURL.substr(-1) === '/') {
-            inputURL = inputURL.substr(0, inputURL.length - 1); // eslint-disable-line no-param-reassign
-        }
+        const room = inputURL.substr(-1) === '/' ? inputURL.substr(0, inputURL.length - 1) : inputURL;
 
         // To avoid potential phishing attacks, we allow only room to
         // be sent from the app link and use server from app settings.
-        if (inputURL.includes('/')) {
-            room = inputURL.split('/').pop();
-        } else {
-            room = inputURL;
-        }
+        // if (inputURL.includes('/')) {
+        //     room = inputURL.split('/').pop();
+        // } else {
+        //     room = inputURL;
+        // }
 
         const conference = createConferenceObjectFromURL(room);
 
